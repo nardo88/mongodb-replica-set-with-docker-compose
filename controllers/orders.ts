c5c675d8e3accdd6a9290eed35b5b8c5fd996e19
nardo88 <nardo1988@mail.ru>
2024-11-10 13:35:18 +0300

    1

--- controllers/orders.ts
+++ controllers/orders.ts
@@ -6,45 +6,19 @@ import { Request, Response } from "express";
 import mongoose from "mongoose";
 
 export class OrderController {
-  // async add(req: Request, res: Response) {
-  //   let session = await mongoose.startSession();
-  //   try {
-  //     session.startTransaction();
-  //     const { userId, thingId } = req.body;
-
-  //     const _id = createId();
-  //     const order = new Orders({
-  //       userId,
-  //       thingId,
-  //       _id,
-  //     });
-
-  //     await order.save({ session });
-
-  //     errorGenerator.typeError(0);
-
-  //     await session.commitTransaction();
-
-  //     return res.sendStatus(201);
-  //   } catch (e: any) {
-  //     console.log(2);
-
-  //     session.abortTransaction();
-  //     res.status(500).json({ message: e?.message });
-  //   } finally {
-  //     session.endSession();
-  //   }
-  // }
+  // SAVE =================================================================
   async add(req: Request, res: Response) {
     let session = await mongoose.startSession();
     try {
       session.startTransaction();
       const { userId, thingId } = req.body;
+
       const thing = await Things.findOne({ _id: thingId });
       if (!thing) return res.status(404).json({ message: "not found" });
 
-      if (thing.total === 0)
+      if (thing.total === 0) {
         return res.status(404).json({ message: "thing is empty" });
+      }
 
       const _id = createId();
       const order = new Orders({
@@ -55,17 +29,16 @@ export class OrderController {
 
       await order.save({ session });
 
+      // errorGenerator.typeError(0); // Генерируем ошибку
+
+      // @ts-ignore
+      // thing.total = "null";
       thing.total = thing.total - 1;
       await thing.save({ session });
 
-      // errorGenerator.typeError(0);
-
       await session.commitTransaction();
-
       return res.sendStatus(201);
     } catch (e: any) {
-      console.log(2);
-
       session.abortTransaction();
       res.status(500).json({ message: e?.message });
     } finally {
@@ -73,3 +46,68 @@ export class OrderController {
     }
   }
 }
+
+// insertMany ===========================================================================
+// метод create напрямую не поддерживает транзакции
+// в Mongoose метод insertOne не является рекомендуемым способом для создания одного документа. ЛУчший вариант - save
+
+// ДРУГИЕ МЕТОДЫ =============================================
+
+// await Orders.deleteOne(
+//   { /* критерий для удаления одного документа */ },
+//   { session }
+// );
+
+// await Orders.deleteMany(
+//   { /* критерий для удаления нескольких документов */ },
+//   { session }
+// );
+
+// await Orders.insertMany(
+//   [
+//     {
+//       _id: createId(),
+//       userId,
+//       thingId,
+//     },
+//     {
+//       _id: createId(),
+//       userId,
+//       thingId,
+//     },
+//   ],
+//   { session }
+// );
+
+// Обновление одного документа с использованием updateOne в транзакции
+//   await Orders.updateOne(
+//     { /* критерий для поиска документа */ },
+//     { $set: { /* данные для обновления */ } },
+//     { session }
+// );
+
+// Обновление одного документа с использованием findOneAndUpdate в транзакции
+// await Orders.findOneAndUpdate(
+//   {
+//     /* критерий для поиска документа */
+//   },
+//   {
+//     $set: {
+//       /* данные для обновления */
+//     },
+//   },
+//   { session, new: true } // `new: true` возвращает обновленный документ
+// );
+
+// Обновление нескольких документов с использованием updateMany в транзакции
+// await Orders.updateMany(
+//   {
+//     /* критерий для поиска документов */
+//   },
+//   {
+//     $set: {
+//       /* данные для обновления */
+//     },
+//   },
+//   { session }
+// );
